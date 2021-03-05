3d0dd5eef429f0f576ea2e2529eecb125b75acc8
Martin M <maroymart@gmail.com>
2021-03-05 05:06:43 +0000

    Added Game and Canvas classes

---
+++ src/game.ts
@@ -0,0 +1,9 @@
+/* tslint:disable */
+import { Canvas } from './ux/index.js';
+/* tslint:enable */
+
+export class Game {
+  static init() {
+
+  }
+}
---
+++ src/ux/index.ts
@@ -0,0 +1,5 @@
+/* tslint:disable */
+import { Canvas } from './canvas.js';
+/* tslint:enable */
+
+export { Canvas };
