182f398b694af6ea6066e7de54aead9a4691e934
Martin M <maroymart@gmail.com>
2021-03-05 17:07:06 +0000

    More player stuff

--- src/objects/player.ts
+++ src/objects/player.ts
@@ -5,15 +5,51 @@ import { Game } from '../game.ts';
 // @ts-ignore
 import { IDrawable } from '../types/gameobjects.ts';
 // @ts-ignore
-import { Board } from '../ux/index.ts';
+import { Board, Canvas } from '../ux/index.ts';
 
 export class Player implements IPlayerObject {
   public speed: Speed = Speed.NORMAL;
+  public move_distance: number = 10;
   public direction: Direction = Direction.NONE;
   public position: Position;
 
   constructor(position: Position) {
     this.position = position;
 
+    Board.place_object(this, position);
   }
+
+  public move() {
+    const position: Position = Position.copy(this.position);
+
+    switch(this.direction) {
+      case Direction.UP:
+        position.Y += this.move_distance;
+        break;
+      case Direction.DOWN:
+        position.Y -= this.move_distance;
+        break;
+      case Direction.LEFT:
+        position.X -= this.move_distance;
+        break;
+      case Direction.RIGHT:
+        position.X += this.move_distance;
+        break;
+    }
+
+    this.update_board(position);
+  }
+
+  public on_hit_screen_edge(edge: ScreenEdge) {
+		switch (edge) {
+			case ScreenEdge.NORTH:
+        this.position.Y = Canvas.height;
+			case ScreenEdge.SOUTH:
+        this.position.Y = 0;
+			case ScreenEdge.EAST:
+        this.position.X = 0
+			case ScreenEdge.WEST:
+        this.position.X = Canvas.width;
+		}
+	}
 }
