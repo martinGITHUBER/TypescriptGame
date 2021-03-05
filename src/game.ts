206594375e821db1359608595065aefd72ab2e50
Martin M <maroymart@gmail.com>
2021-03-05 15:38:04 +0000

    Testing timer

--- src/game.ts
+++ src/game.ts
@@ -1,10 +1,14 @@
 // @ts-ignore
 import { Canvas } from './ux/index.ts';
+// @ts-ignore
+import { ClockTick, Timer, Direction } from './types/index.ts'
 
 export class Game {
   static init() {
     Canvas.init(<HTMLCanvasElement>document.querySelector("#canvas"));
     Canvas.fill("black");
+
+    const timer = new Timer(5000, 0, alert("hi"));
   }
 }
 
--- src/types/index.ts
+++ src/types/index.ts
@@ -2,8 +2,11 @@
 import { GameKey, ScreenEdge, Direction, Speed } from './enums.ts';
 // @ts-ignore
 import { Position } from './position.ts';
+// @ts-ignore
+import { ClockTick, ClockType, Timer } from './timer.ts' 
 
 export {
   GameKey, ScreenEdge,
-  Direction, Speed
+  Direction, Speed,
+  ClockTick, ClockType, Timer
 }
