d67f96fe396446764bcf681afd19949c4bb1f4b1
Martin M <maroymart@gmail.com>
2021-03-05 21:43:54 -0500

    The cold truth hits me

--- src/objects/index.ts
+++
@@ -1,4 +0,0 @@
-// @ts-ignore
-import { Player } from './player.ts';
-
-export { Player };
--- src/ux/index.ts
+++
@@ -1,8 +0,0 @@
-// @ts-ignore
-import { Canvas } from './canvas.ts';
-// @ts-ignore
-import { Board } from './board.ts';
-// @ts-ignore
-import { Controls } from './controls.ts';
-
-export { Canvas, Board, Controls };
