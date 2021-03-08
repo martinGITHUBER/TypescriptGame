bd752eed293b3b21a17318e8c64fb2cab15aaf76
Martin M <maroymart@gmail.com>
2021-03-08 00:15:41 -0500

    Time to start socket.io stuff

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const io = require('socket.io')(server);
 
 const app = express();
 
@@ -7,4 +8,61 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(3000, () => {
 	console.log('server started');
+});
+
+const board = {
+	players: []
+}
+
+io.on('connection', socket => {
+	let player_name = '';
+	let in_game = false;
+	let move_cooldown;
+
+
+	socket.on('join', player => {
+		if(board.players.find(p => p.name = player.name)){
+			return socket.emit("invalid_name");
+		}
+
+		const player_obj = {
+			name: player.name,
+			x: 200,
+			y: 200,
+			speed,
+			direction: 'NONE'
+		};
+
+		board.players.push(player_obj);
+
+		player_name == player.name;
+		in_game = true;
+		socket.emit("joined", player_obj);
+	});
+
+	const interval = setInterval(100, () => {
+		socket.emit("update", board);
+	});
+
+	socket.on('move', direction => {
+		const player = board.players.find(p => p.name == player_name);
+		if(!in_game || !player.can_move) return;
+
+		switch(direction) {
+			case 'UP':
+				player.y += player.speed;
+			case 'DOWN':
+				player.y -= player.speed;
+			
+		}
+
+		player.can_move = false;
+		move_cooldown = setTimeout(() => player.can_move == true, 100)
+	});
+
+	socket.on('disconnect', () => {
+		clearInterval(interval);
+
+		board.players.splice(board.players.indexOf(board.players.find(p => p.name == player.name)), 1);
+	});
 });
